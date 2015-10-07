1794e1a0650244d89c2e229663be3119dea82e69
Fan Yang <yangfan879@gmail.com>
2015-10-07 15:50:36 +0800

    Add support for escaped *

--- tree/tree.go
+++ tree/tree.go
@@ -3,9 +3,8 @@ package tree
 type nodeType int
 
 const (
-	oNode nodeType = iota // ordinary
-	wNode                 // wildcard
-	eNode                 // end
+	cNode nodeType = iota // common
+	wNode                 // wildcard character
 )
 
 type Tree struct {
@@ -13,18 +12,34 @@ type Tree struct {
 }
 
 type Node struct {
-	child []*Node
-	index []byte // first byte of each child
-	s     string
-	v     interface{}
-	typ   nodeType
+	child    []*Node
+	childidx []byte // first byte of each child
+	wcard    *Node
+	s        string
+	v        interface{}
+	typ      nodeType
+	end      bool
 }
 
-func newSubtree(pattern string, v interface{}) *Node {
+func newTree(pattern string, v interface{}) *Node {
 	var root, n, child *Node
 	var j int
 	for i := 0; i < len(pattern); {
-		for j = 0; j < len(pattern[i:]) && pattern[i+j] != '*'; j++ {
+		s, escape := []byte{}, false
+	FIND_AST:
+		for j = 0; j < len(pattern[i:]); j++ {
+			switch pattern[i+j] {
+			case '\\':
+				if escape = !escape; escape {
+					continue FIND_AST
+				}
+			case '*':
+				if !escape {
+					break FIND_AST
+				}
+			}
+			escape = false
+			s = append(s, pattern[i+j])
 		}
 		switch j {
 		case 0:
@@ -35,87 +50,170 @@ func newSubtree(pattern string, v interface{}) *Node {
 			i++
 		default:
 			child = &Node{
-				s:   pattern[i : i+j],
-				typ: oNode,
+				s:   string(s),
+				typ: cNode,
 			}
 			i = i + j
 		}
 		if n != nil {
-			n.child = []*Node{child}
-			n = child
+			switch child.typ {
+			case wNode:
+				n.wcard = child
+			case cNode:
+				n.child = []*Node{child}
+				n.childidx = []byte{child.s[0]}
+			}
 		} else {
-			n = child
-			root = n
+			root = child
 		}
+		n = child
 	}
 	n.v = v
+	n.end = true
 	return root
 }
 
-// panic("a registered handler conflicts with pattern: " + pattern[:i+l])
-func (t *Tree) Insert(pattern string, v interface{}) {
+func (n *Node) setV(v interface{}) (ov interface{}, is bool) {
+	ov, is = n.v, n.end
+	n.v, n.end = v, true
+	return
+}
+
+func (t *Tree) Add(pattern string, v interface{}) (ov interface{}, is bool) {
+	if pattern == "" {
+		return
+	}
 	if t.root == nil {
-		t.root = newSubtree(pattern, v)
+		t.root = newTree(pattern, v)
 		return
 	}
 	n := t.root
 INSERT:
 	for {
-		minLen := len(pattern)
-		if minLen > len(n.s) {
-			minLen = len(n.s)
-		}
-		var l int // length of longest common prefix
-		for l = 0; l < minLen && pattern[l] == n.s[l]; l++ {
-		} // at the end of loop: pattern[:l] == n.s[:l]
-		switch l {
-		case len(n.s): // totally match this node
-			pattern = pattern[l:]
-			if len(pattern) == 0 { // end
-				n.v = v
-				break INSERT
+		var i, l int
+		var wmatch, escape bool
+
+		if n.typ == wNode {
+			if len(pattern) > 0 && pattern[0] == '*' {
+				wmatch = true
 			}
-			// go down
-			var k int
-			for k = 0; k < len(n.child); k++ {
-				if n.child[k].s[0] == pattern[0] {
-					break
+			goto SWITCH
+		}
+
+		for i < len(pattern) && l < len(n.s) {
+			if pattern[i] == '\\' {
+				if escape = !escape; escape {
+					i++
+					continue
 				}
 			}
-			if k != len(n.child) { // found
-				n = n.child[k]
-				continue INSERT
-			} // else not found
-		default: // split
-			child := &Node{
-				s:     n.s[l:],
-				typ:   oNode,
-				child: n.child,
+			if !escape && pattern[i] == '*' {
+				break
 			}
-			n.s = n.s[:l]
-			n.child = []*Node{child}
-			pattern = pattern[l:]
+			if pattern[i] != n.s[l] {
+				break
+			}
+			escape = false
+			i, l = i+1, l+1
+		}
+		if escape {
+			i--
+			escape = false
+		}
+	SWITCH:
+		switch {
+		case wmatch:
+			i = 1
+			fallthrough
+		case l == len(n.s): // totally match this node
+			pattern = pattern[i:]
 			if len(pattern) == 0 { // end
-				n.v = v
-				break INSERT
+				return n.setV(v)
+			}
+			if pattern[0] == '*' {
+				if n.wcard == nil {
+					n.wcard = newTree(pattern, v)
+					return
+				} else {
+					n = n.wcard
+					continue INSERT
+				}
+			}
+
+			first := 0
+			if pattern[0] == '\\' {
+				first = 1
+			}
+			if len(pattern[first:]) > 0 {
+				for i := 0; i < len(n.childidx); i++ {
+					if n.childidx[i] == pattern[first] {
+						n = n.child[i]
+						continue INSERT
+					}
+				}
+			}
+			// not found
+		case n.typ == wNode:
+			i, l = 0, 0
+			fallthrough
+		default: // split
+			prefix, suffix := n.s[:l], n.s[l:]
+			child := &Node{
+				s:        suffix,
+				typ:      n.typ,
+				child:    n.child,
+				childidx: n.childidx,
+				wcard:    n.wcard,
+			}
+			*n = Node{}
+			n.s = prefix
+			n.typ = cNode
+			if child.typ == wNode {
+				n.wcard = child
+			} else {
+				n.child = []*Node{child}
+				n.childidx = []byte{child.s[0]}
+			}
+			pattern = pattern[i:]
+			if len(pattern) == 0 { // end
+				return n.setV(v)
 			}
 		}
 		// construct a new subtree using rest of pattern and
 		// append it to the child list of this node
-		n.child = append(n.child, newSubtree(pattern, v))
-		break INSERT
+		child := newTree(pattern, v)
+		switch child.typ {
+		case cNode:
+			n.child = append(n.child, child)
+			n.childidx = append(n.childidx, child.s[0])
+		case wNode:
+			n.wcard = child
+		}
+		return
 	}
 }
 
-func (t *Tree) Lookup(s string) *Node {
-	return lookup(t.root, s)
+func (t *Tree) Lookup(s string) (v interface{}, ok bool) {
+	n := lookup(t.root, s)
+	if n != nil {
+		v, ok = n.v, n.end
+	}
+	return
 }
 
 func lookup(n *Node, s string) *Node {
 	if n == nil {
 		return nil
 	}
-	var found *Node
+	if n.typ == wNode {
+		for capture := 0; capture <= len(s); capture++ {
+			if end := lookupW(n, s[capture:]); end != nil {
+				return end
+			}
+		}
+		return nil
+	}
+
 	minLen := len(s)
 	if minLen > len(n.s) {
 		minLen = len(n.s)
@@ -127,57 +225,42 @@ func lookup(n *Node, s string) *Node {
 	case len(n.s): // totally match this node
 		s = s[l:]
 		if len(s) == 0 { // end
+			if end := lookup(n.wcard, s); end != nil {
+				return end
+			}
 			return n
 		}
 		// go down
 		var k int
 		for k = 0; k < len(n.child); k++ {
 			if n.child[k].s[0] == s[0] {
-				found = lookup(n.child[k], s)
+				if end := lookup(n.child[k], s); end != nil {
+					return end
+				}
 				break
 			}
 		}
-		if found != nil {
-			return found
-		}
 		// try '*'
-		for k = 0; k < len(n.child); k++ {
-			if n.child[k].s[0] == '*' {
-				return lookup(n.child[k], s)
-			}
-		}
-		return nil
+		return lookup(n.wcard, s)
 	default:
-		if l != 0 || n.s[0] != '*' {
-			return nil
-		}
-		if len(s) == 0 {
-			return n
-		}
-		for catch := 0; catch < len(s); catch++ {
-			if found = lookupW(n, s[catch:]); found != nil {
-				return found
-			}
-		}
 		return nil
 	}
 }
 
+// n must be a wildcard node
 func lookupW(n *Node, s string) *Node {
-	var found *Node
-	for i := 0; i < len(n.child); i++ {
-		if n.child[i].s[0] == s[0] {
-			if found = lookup(n.child[i], s); found != nil {
-				return found
+	if s == "" {
+		return n
+	}
+	var end *Node
+	for i := 0; i < len(n.childidx); i++ {
+		if n.childidx[i] == s[0] {
+			if end = lookup(n.child[i], s); end != nil {
+				return end
 			}
 			break
 		}
 	}
 	// try '*'
-	for i := 0; i < len(n.child); i++ {
-		if n.child[i].s[0] == '*' {
-			return lookup(n.child[i], s)
-		}
-	}
-	return nil
+	return lookup(n.wcard, s)
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -6,35 +6,41 @@ import (
 )
 
 func TestSiteTree(t *testing.T) {
-	testIn := []string{
-		"*abcd*ef*",
-		"*.google.com",
-		"http://example.com/books/*",
-		"*://example.com/movies",
-		"http://example.com/",
+	testIn := []struct {
+		s string
+		i interface{}
+	}{
+		{"*abcd*ef*", 1},
+		{"*.google.com", 2},
+		{"http://example.com/books/*", 3},
+		{"*://example.com/movies", 4},
+		{`http://example.com/\*`, 5},
+		{`http://example.com/*`, 6},
 	}
 	test := []struct {
-		s     string
-		found bool
+		s string
+		v interface{}
 	}{
-		{"abcdef", true},
-		{"abcdefef", true},
-		{"abcabcdefgef", true},
-		{"google.com", false},
-		{"www.google.com", true},
-		{"http://example.com/books/", true},
+		{"abcdef", 1},
+		{"abcdefef", 1},
+		{"abcabcdefgef", 1},
+		{"google.com", nil},
+		{"www.google.com", 2},
+		{"http://example.com/books/", 3},
+		{"http://example.com/", 6},
+		{"http://example.com/*", 5},
 	}
 
 	tr := &Tree{}
 	for _, s := range testIn {
-		tr.Insert(s, nil)
-		// fmt.Println("After insert", s)
-		// printSibling(tr.root)
+		tr.Add(s.s, s.i)
 	}
 
 	for _, data := range test {
-		if found := tr.Lookup(data.s); (found != nil) != data.found {
-			t.Errorf("lookup %q failed: expect %v, got %v\n", data.s, data.found, !data.found)
+		// fmt.Println("Lookup", data.s)
+		v, ok := tr.Lookup(data.s)
+		if !ok && v != data.v {
+			t.Errorf("lookup %q failed: expect %v, got %v", data.s, data.v, v)
 		}
 	}
 }
@@ -44,8 +50,14 @@ func printSibling(node *Node) {
 	for _, n := range node.child {
 		fmt.Printf("%s ", n.s)
 	}
+	if node.wcard != nil {
+		fmt.Printf("*-")
+	}
 	fmt.Println("")
 	for _, n := range node.child {
 		printSibling(n)
+	}
+	if node.wcard != nil {
+		printSibling(node.wcard)
 	}
 }
