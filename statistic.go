3c638b4e87c4b5da45e729e0506ac94afbe3cccc
Fan Yang <yangfan879@gmail.com>
2015-10-22 22:02:52 +0800

    Fix a race condition

--- statistic.go
+++ statistic.go
@@ -17,7 +17,5 @@ func (s *Statistic) IncAllCount()   { atomic.AddInt32(&s.All, 1) }
 func (s *Statistic) IncTimesCount() { atomic.AddInt32(&s.Times, 1) }
 func (s *Statistic) IncErrorCount() { atomic.AddInt32(&s.Errors, 1) }
 func (s *Statistic) IncDoneCount() (alldone bool) {
-	atomic.AddInt32(&s.Done, 1)
-	// fmt.Printf("all: %d, done: %d\n", atomic.LoadInt32(&s.URLs), atomic.LoadInt32(&s.Done))
-	return atomic.LoadInt32(&s.All) == atomic.LoadInt32(&s.Done)
+	return atomic.LoadInt32(&s.All) <= atomic.AddInt32(&s.Done, 1)
 }
